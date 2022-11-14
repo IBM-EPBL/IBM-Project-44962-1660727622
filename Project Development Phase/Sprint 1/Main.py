206a57e491d4524181bb94444921f2be9def61ab
Maheswarivel <113621454+Maheswarivel@users.noreply.github.com>
2022-11-14 20:44:01 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 1/Main.py
@@ -0,0 +1,33 @@
+# Python code
+
+# IMPORT SECTION STARTS
+
+import brain
+
+# IMPORT SECTION ENDS
+# -----------------------------------------------
+# USER INPUT SECTION STARTS
+
+myLocation = "Thanjavur,TN"
+APIKEY = "e7faf1fb73b0914c60d2e8ceeb810262"
+
+localityInfo = {
+    "schools" : {
+        "schoolZone" : True,
+        "activeTime" : ["7:00","17:30"] # schools active from 7 AM till 5:30 PM
+        },
+    "hospitalsNearby" : False,
+    "usualSpeedLimit" : 40 # in km/hr
+}
+
+# USER INPUT SECTION ENDS
+# -----------------------------------------------
+# MICRO-CONTROLLER CODE STARTS
+
+print(brain.processConditions(myLocation,APIKEY,localityInfo))
+
+'''
+MICRO CONTROLLER CODE WILL BE ADDED IN SPRINT 2 AS PER OUR PLANNED SPRINT SCHEDULE
+'''
+
+# MICRO-CONTROLLER CODE ENDS
